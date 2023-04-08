ad8c42985153d0bf670cbaf44a9a42d630d8440b
Emerson Moreira Neutzling <emerson.neutzling@gmail.com>
2023-04-08 19:38:26 -0300

    atualizei javascript

--- .script.js
+++ .script.js
@@ -1,4 +1,5 @@
-const menuClick = document.querySelector('.bg header i');
+function MENUCLICK() {
+    const menuClick = document.querySelector('.bg header i');
 const nav = document.querySelector('.bg header nav ul');
 
 menuClick.addEventListener('click', clicou);
@@ -6,3 +7,25 @@ menuClick.addEventListener('click', clicou);
 function clicou() {
     nav.classList.toggle('ativo');
 }
+
+}
+
+MENUCLICK();
+
+const lista = document.querySelectorAll('.js-texto p');
+const listaIcone = document.querySelectorAll('.js-texto i');
+lista[0].classList.add('ativo')
+
+
+function clickLista(index) {
+    lista.forEach((item) => {
+        item.classList.remove('ativo');
+    });
+    lista[index].classList.add('ativo');
+}
+
+listaIcone.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        clickLista(index)
+    })
+})
